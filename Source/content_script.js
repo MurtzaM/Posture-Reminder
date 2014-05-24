b9d4f8f437459959e7de6ce66c21192418abad73
Murtza Manzur <Hi@Murtza.org>
2014-05-24 16:57:02 -0700

    Init commit

---
+++ Source/content_script.js
@@ -0,0 +1,60 @@
+walk(document.body);
+
+if (window.MutationObserver) {
+	var observer = new MutationObserver(function (mutations) {
+		Array.prototype.forEach.call(mutations, function (m) {
+			if (m.type === 'childList') {
+				walk(m.target);
+			} else if (m.target.nodeType === 3) {
+				handleText(m.target);
+			}
+		});
+	});
+
+	observer.observe(document.body, {
+		childList: true,
+		attributes: false,
+		characterData: true,
+		subtree: true
+	});
+}
+
+function walk(node) 
+{
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var oldValue = textNode.nodeValue;
+	    v = oldValue;
+
+	v = v.replace(/\bThe Cloud\b/g, "My Butt");
+	v = v.replace(/\bThe cloud\b/g, "My butt");
+	v = v.replace(/\bthe Cloud\b/g, "my Butt");
+	v = v.replace(/\bthe cloud\b/g, "my butt");
+	
+	// avoid infinite series of DOM changes
+	if (v !== oldValue) {
+		textNode.nodeValue = v;
+	}
+}
